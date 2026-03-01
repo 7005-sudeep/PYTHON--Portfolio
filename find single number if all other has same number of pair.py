9a3331356bf7404e2a1d374f4efe4d09f9c12a29
7005-sudeep <saikakade501@gmail.com>
2026-03-01 11:24:07 -0500

    Create find single number if all other has same number of pair.py

---
+++ find single number if all other has same number of pair.py
@@ -0,0 +1,15 @@
+def find_single_number(nums):
+    # Initialize our "Accumulator" to 0
+    # In hardware: res = 0;
+    res = 0
+    
+    for x in nums:
+        # Apply XOR: res = res ^ x
+        res =res ^ x
+        
+    return res
+
+# Test Case (A stream of Transaction IDs)
+# Notice: 2 appears twice, 3 appears twice. 4 is unique.
+bus_ids = [2, 3, 2, 4, 3]
+print(f"The unique Transaction ID is: {find_single_number(bus_ids)}")
